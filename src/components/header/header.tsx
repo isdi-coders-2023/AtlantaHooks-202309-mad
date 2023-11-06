73029bc3b0f325d5b466d00a67064a720d76ed03
Serafin Alferez <malaga.carbon3@gmail.com>
2023-11-06 16:14:51 +0100

    Add header.test.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,12 @@
-import { useContext } from 'react';
+
 import './header.scss';
-import { Context } from '../../context/context'
 
-type Props = {
-  title: string;
-  children: JSX.Element;
-};
-export function Header({ title, children }: Props) {
-
+export function Header() {
   return (
     <header>
       <h1>
-        {title}
+        
       </h1>
-      {children}
     </header>
   );
 }
