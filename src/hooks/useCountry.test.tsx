1ac1fe5f212af6559c6ef6d9cae38e44cc90fc72
Bruno Coelho <bruno100vc@hotmail.com>
2023-11-08 20:33:13 +0100

    Add new test for coverage

--- src/hooks/useCountry.test.tsx
+++ src/hooks/useCountry.test.tsx
@@ -6,6 +6,7 @@ jest.mock('../services/repo', () => ({
     getCountry: jest.fn(() => Promise.resolve([])),
   })),
 }));
+
 describe('Given useCountry custom Hook', () => {
   describe('useCountry Hook', () => {
     it('should load countries and update state', async () => {
@@ -15,12 +16,29 @@ describe('Given useCountry custom Hook', () => {
       });
       expect(result.current.countriesState.country).toHaveLength(0);
     });
+
     it('should change the page correctly', () => {
       const { result } = renderHook(() => useCountry());
       act(() => {
         result.current.handleChangePage(1);
       });
       expect(result.current.countriesState.page).toBe(2);
+      act(() => {
+        result.current.handleChangePage(-1);
+      });
+      expect(result.current.countriesState.page).toBe(1);
+    });
+
+    it('should handle increment greater than 1 correctly', () => {
+      const { result } = renderHook(() => useCountry());
+      act(() => {
+        result.current.handleChangePage(2);
+      });
+      expect(result.current.countriesState.page).toBe(3);
+    });
+
+    it('should handle decrement that goes below the minimum page correctly', () => {
+      const { result } = renderHook(() => useCountry());
       act(() => {
         result.current.handleChangePage(-1);
       });
