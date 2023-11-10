e15ab40fc00bb2a46543e99f8affd798f42e0bcc
Camilo Isaza <camiloisazag@hotmail.com>
2023-11-10 17:36:13 +0100

    Add initial configs for private API

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -4,7 +4,10 @@ import { ActionCountry } from './actions';
 export type AppState = {
   country: Country[];
   page: number;
+  privateCountry: Country[];
+  privatePage: number;
 };
+
 export function countryReducer(
   state: AppState,
   { type, payload }: ActionCountry
@@ -14,6 +17,10 @@ export function countryReducer(
       return { ...state, country: payload };
     case 'changePage':
       return { ...state, page: payload };
+    case 'privateLoad':
+      return { ...state, privateCountry: payload };
+    case 'changePrivatePage':
+      return { ...state, privatePage: payload };
     default:
       return { ...state };
   }
